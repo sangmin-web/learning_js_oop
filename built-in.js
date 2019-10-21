be6861e6a48d8aa0c175afa1b4e02df0077d4878
sangmin <mytkdals93@gmail.com>
2019-10-21 11:47:08 +0900

    4.2 객체 만들어 보기

--- built-in.js
+++ built-in.js
@@ -5,3 +5,18 @@
 console.log(Math.PI);
 console.log("Math.random()", Math.random());
 console.log("Math.floor(3.9)", Math.floor(3.9));
+
+//객체의 소속된 함수 - 메소드
+
+var MyMath = {
+    PI: Math.PI,
+    random: function () {
+        return Math.random() * 10;
+    },
+    floor: function (val) {
+        return Math.floor(val)
+    }
+}
+console.log("MyMath.PI", MyMath.PI);
+console.log("MyMath.random()", MyMath.random());
+console.log("MyMath.floor(3.9)", MyMath.floor(3.9));
