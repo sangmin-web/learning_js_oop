66fe933bfceeb1aaf449a91cd29b45532a6b6016
이상민 <56681633+sangmin-web@users.noreply.github.com>
2019-10-23 11:03:49 +0900

    9. class의 constructor function

---
+++ class.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(name, first, second) {
+        //객체가 생성될때 실행되는 메소드!
+        this.name = name;
+        this.first = first;
+        this.second = second;
+    }
+    sum(){
+        //메소드 정의
+        return this.first + this.second;
+    }
+}
+let u1 = new User('상민',12,31);
+console.log(u1)
