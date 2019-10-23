02b18d8caf49a48fe507b59b293ecd95a7234221
이상민 <56681633+sangmin-web@users.noreply.github.com>
2019-10-23 11:51:55 +0900

    14.1 객체와 함수

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -19,3 +19,34 @@ subObj.superVal = 'modify by sub';
 
 console.log('subObj.superVal=>', subObj.superVal);
 console.log('supObj.superVal=>', superObj.superVal);
+
+
+
+let kim = {
+    name: "kim",
+    first: 10,
+    second: 20,
+    sum: function () {
+        return this.first + this.second
+    }
+
+}
+console.log(`kim.sum(): ${kim.sum()}`)
+let lee = Object.create(kim);
+lee.name = 'lee';
+lee.second = 10;
+
+lee.avg = function () {
+    return this.sum() / 2
+}
+//  lee = {
+//     name: "lee",
+//     first: 10,
+//     second: 10,
+//     avg: function(){
+//         return this.sum()/2
+//     }
+// }
+// lee.__proto__ = kim;
+console.log('lee.sum(): ', lee.sum());
+console.log('lee.avg(): ', lee.avg());
