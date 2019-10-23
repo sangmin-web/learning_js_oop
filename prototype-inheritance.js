721247222117831dc376dda0e905165f9fe996a0
이상민 <56681633+sangmin-web@users.noreply.github.com>
2019-10-23 11:39:02 +0900

    13.3. Object.create()

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,13 +1,21 @@
 let superObj = {
     superVal: "super"
 }
-let subObj = {
-    subVal: "sub"
-}
+// let subObj = {
+//     subVal: "sub"
+// }
+// // subObj.__proto__ = superObj;
+// //먼저 subObj에서 superVal을 찾고 없으면 위로 올라간다.
+// console.log(subObj.superVal);
+// subObj.superVal = 'modify by sub';
+// console.log('subObj.superVal=>', subObj.superVal);
+// console.log('supObj.superVal=>', superObj.superVal);
 
-subObj.__proto__ = superObj;
-//먼저 subObj에서 superVal을 찾고 없으면 위로 올라간다.
-console.log(subObj.superVal);
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+
+console.log('subObj.subVal=>', subObj.subVal);
 subObj.superVal = 'modify by sub';
+
 console.log('subObj.superVal=>', subObj.superVal);
 console.log('supObj.superVal=>', superObj.superVal);
