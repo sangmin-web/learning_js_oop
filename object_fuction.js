dbd65f759a438e0ea681447711e9c17a52c6a46d
이상민 <56681633+sangmin-web@users.noreply.github.com>
2019-10-23 20:53:25 +0900

    14. call, apply

---
+++ object_fuction.js
@@ -0,0 +1,22 @@
+var kim = {
+    name: 'kim',
+    first: 10,
+    second: 20
+};
+var lee = {
+    name: 'lee',
+    first: 10,
+    second: 10
+};
+//어떤 객체에도 속하지 않은 함수
+function sum(a, b, c) {
+    console.log(a, b, c);
+    return (this.first + this.second);
+}
+
+//fuc.call(객체, 인자...)
+console.log("sum.call(kim): ", sum.call(kim, kim.name, "a", "z"));
+console.log("sum.call(lee): ", sum.call(lee, lee.name, "b", "c"));
+//func.apply(객체,[인자...])
+console.log("sum.apply(kim): ", sum.apply(kim, [kim.name, "a", "z"]));
+console.log("sum.apply(lee): ", sum.apply(lee, [lee.name, "b", "c"]));
